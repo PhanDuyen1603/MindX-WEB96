e9b912b283b650cca8ca9d8456f350334051c134
May <22424005@student.hcmus.edu.vn>
2026-05-08 21:57:29 +0700

    update SS02

---
+++ SS02/server/index.js
@@ -0,0 +1,63 @@
+// import http from 'http';
+import express from 'express';
+import crypto from 'crypto';
+// import data from './data.js';
+import { customers } from './data.js';
+const app = express();
+app.use(express.json());
+
+
+// tạo server
+const PORT = 8080;
+
+app.get('/customers', (req, res) => {
+    res.json(customers);
+});
+
+app.get('/customers/:id', (req, res) => {
+    const { id } = req.params;// lấy id từ params //C001
+    const customer = customers.find(c => c.id === id); //C001 = C001
+    if (!customer) {
+        return res.status(404).json({ error: 'Customer not found' });
+    }
+    res.json(customer);
+});
+
+app.post('/customers', (req, res) => {
+    const { name, email, age } = req.body; // lấy dữ liệu từ body
+
+    //validate data
+    if (!name || !email || age === undefined) {
+        return res.status(400).json({ error: 'Name, email and age are required' });
+    }
+
+    const isEmailUsed = customers.some(c => c.email === email);
+    if (isEmailUsed) {
+        return res.status(400).json({ error: 'Email already used' });
+    }
+
+    //create new customer 
+    //Tao sinh ngau nhien id
+    const id = crypto.randomUUID(); // tạo id ngẫu nhiên
+    const newCustomer = { id, name, email, age };
+    console.log(newCustomer); // tạo customer mới
+    customers.push(newCustomer); // thêm customer mới vào customers
+    res.status(201).json(newCustomer); // trả về customer mới
+});
+
+app.delete('/customers/:id', (req, res) => {
+    const { id } = req.params;
+    const index = customers.findIndex(c => c.id === id);
+    if (index === -1) {
+      return res.status(404).json({ error: 'Customer not found' });
+    }
+    customers.splice(index, 1);
+    return res.status(200).json({ message: 'Customer deleted successfully' });
+  });
+
+app.listen(PORT, () => {
+    console.log(`Server is running`);
+});
+
+
+
